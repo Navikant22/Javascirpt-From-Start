450011ff05b903d8514661d00cd0e599d117337f
Navikant22 <142095492+Navikant22@users.noreply.github.com>
2026-02-28 11:15:11 +0530

    Refactor variable declarations in 01_Variable.js
    Updated variable declarations and values for account details.

--- 01_Variable.js
+++ 01_Variable.js
@@ -1,1 +1,18 @@
+const accountId=15515    //you can't change
+let accountEmail="navikant@gmail.com" //use only let instead of var
+var accountPassword="1234321"
+accountCity="Noida"
+// accountId = 2 not allowed to change
+accountEmail = "rahaul@gmail.com"
+accountPassword="123456"
+accountCity="jaypur" // this not way not initialize
+let accountState;
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity ,accountState]) 
+/*
+prefer not use var 
+bacause of issue in block scope and functional 
+*/
+
 
