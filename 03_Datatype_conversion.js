7cec75f2879a32d6dbf2f18e98fb77478427b8e4
Navikant22 <142095492+Navikant22@users.noreply.github.com>
2026-02-28 11:32:53 +0530

    Implement datatype conversion examples
    Add examples of datatype conversion in JavaScript.

---
+++ 03_Datatype_conversion.js
@@ -0,0 +1,24 @@
+// let score=true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+// let vauleNumber = Number(score)
+
+// console.log( vauleNumber);
+// console.log(typeof(vauleNumber));
+
+// "33" = 33
+// "33abc" => NaN
+// true => 1; false => 0
+let isLoggedIn =""
+let booleanIsloggedIn=Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+//  1=> true 0 => false
+//  "" => false
+//  "navi" =>true
+let someNuber=33
+let stringNumer=String(someNuber)
+console.log(stringNumer);
+console.log(typeof stringNumer);
+
+
