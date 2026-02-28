5e5a293b5dade71305866dee229716e2381a5a0a
Navikant22 <142095492+Navikant22@users.noreply.github.com>
2026-02-28 11:16:24 +0530

    Implement basic data types example in 02_DataTypes.js
    Added JavaScript code demonstrating data types and console logging.

---
+++ 02_DataTypes.js
@@ -0,0 +1,24 @@
+"use stict "; //treat all JS code as newer version
+// alert("hello");
+// alret(3+8) // we are using nodejs , not browser
+console.log(8+
+    5 ) // code readabilti should be high
+console.log("Hitesh");
+let name ="Navi" //String
+let age=25 //number
+let isLoggedIn=false //boolean
+let s=null
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean =>true/false
+null => standalone value
+undefined =>
+    // Object
+
+*/
+
+console.log(typeof(s));
+
+
