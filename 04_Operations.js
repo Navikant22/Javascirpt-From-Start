fbbd687b884c52c13ec70a836dfbe7b54cc98e8d
Navikant22 <142095492+Navikant22@users.noreply.github.com>
2026-02-28 12:10:52 +0530

    Implement basic operations in 04_Operations.js
    Added basic arithmetic operations and comments for reference.

---
+++ 04_Operations.js
@@ -0,0 +1,26 @@
+let value=3
+let negValue=-value
+// console.log(negValue);
+// console.log(2+4);
+// console.log(2-4);
+// console.log(2*4);
+// console.log(3%2);
+// console.log(2/4);
+// let str1="Hello"
+// let str2=" Navi"
+// let str3=str1+str2
+// console.log(str3);
+// console.log(1 + "2");
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((2 * 7) + (8 - 9));
+// console.log(+true);
+let counter=100
+++counter
+console.log(counter);
+// Study about pre and post on MDN website
+
+
+
+
