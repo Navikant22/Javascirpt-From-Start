da134d0a23e41a1970d5183a036bb6e160964c38
Navikant22 <142095492+Navikant22@users.noreply.github.com>
2026-03-03 11:27:51 +0530

    Create 06_Datatypes_summary.js with data types overview
    Added summary of primitive and non-primitive data types in JavaScript.

---
+++ 06_Datatypes_summary.js
@@ -0,0 +1,33 @@
+//primitive
+
+
+
+
+// 1 String : Number , Boolean , null , undefined ,Symbol:
+//    BigInt
+// const score =100
+// const scoerValue=100.3
+// const usLoggedIn=false
+// const outsideTemp=null
+// let userEmail;
+// const id=Symbol('1223')
+// const anotherid=Symbol('1223')
+// console.log(id==anotherid);
+// console.table([scoerValue , score ,usLoggedIn ,
+//      ,
+// ])
+
+// Non Primitive 7 types ( reference type)
+
+
+// Array , Objects , Function ,
+const heros = ["Shaktiman " , "Naagraj ", " doga"]
+
+let myvalue={
+    name:"Navikant",
+    age: 22,
+}
+const myFunction = function(){
+    console.log("Hello wrold");
+    
+}
