50f3fa7c89a51b58b7ed1b00cad7e9be415dbbe1
Navikant22 <142095492+Navikant22@users.noreply.github.com>
2026-03-03 12:05:35 +0530

    Add stack and heap memory demonstration in JavaScript
    Demonstrate stack and heap memory concepts with examples.

---
+++ 07_Stack_Heap_inMemmory.js
@@ -0,0 +1,18 @@
+// Stack(Primitive) and Heap(Non Primitive) Memmory
+let myname="Navikant"
+console.log(typeof myname);
+let anothername = myname  
+console.log(anothername)
+anothername = "Chai and code ";
+console.log(anothername);
+let use= {
+    email : "navikant@gmail.com",
+    user :"user$ybl"
+}
+let usetwo=use
+console.log(usetwo);
+usetwo.email="navi@google.com"
+console.log(usetwo);
+
+
+
